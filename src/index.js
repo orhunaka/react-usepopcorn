e40149d7ceb974e20a3f3d9ea30d5c41a6ccf203
Orhun Aka <82637831+orhunaka@users.noreply.github.com>
2024-03-11 08:52:52 +0000

    star rating almost finished

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}></StarRating>
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    ></StarRating>
+    <StarRating
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={0}
+    ></StarRating>
   </React.StrictMode>
 );
